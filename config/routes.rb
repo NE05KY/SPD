7adfa786bcf9363811c91a9b2a154c3aa7ab46b7
NE05KY <leshka.neo@gmail.com>
2013-04-16 15:25:42 +0300

    Pages controller and custom css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Spd::Application.routes.draw do
+  root to: "pages#index"
+
+  get "pages/about"
+  get "pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
